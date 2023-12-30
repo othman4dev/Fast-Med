e6bb4bb5a3cf741b1e6f0f218943f79f93262c15
Anass-Dr <anasdrissi77@gmail.com>
2023-12-30 12:50:40 +0100

    files renamed to .php

--- mvc/views/user/products.php
+++ mvc/views/user/products.php
@@ -1,33 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
 
-  <head>
+<head>
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
 
-    <title>Tale SEO Agency CSS Template by TemplateMo website</title>
+  <title>Tale SEO Agency CSS Template by TemplateMo website</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Bootstrap core CSS -->
+  <link href="user/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
 
-    <!-- Additional CSS Files -->
-    <link rel="stylesheet" href="assets/css/fontawesome.css">
-    <link rel="stylesheet" href="assets/css/templatemo-tale-seo-agency.css">
-    <link rel="stylesheet" href="assets/css/owl.css">
-    <link rel="stylesheet" href="assets/css/animate.css">
-    <link rel="stylesheet"href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
-<!--
+  <!-- Additional CSS Files -->
+  <link rel="stylesheet" href="user/assets/css/fontawesome.css">
+  <link rel="stylesheet" href="user/assets/css/templatemo-tale-seo-agency.css">
+  <link rel="stylesheet" href="user/assets/css/owl.css">
+  <link rel="stylesheet" href="user/assets/css/animate.css">
+  <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+  <!--
 
 TemplateMo 582 Tale SEO Agency
 
 https://templatemo.com/tm-582-tale-seo-agency
 
 -->
-  </head>
+</head>
 
 <body>
 
@@ -75,36 +75,36 @@ https://templatemo.com/tm-582-tale-seo-agency
   <!-- ***** Header Area Start ***** -->
   <header class="header-area header-sticky">
     <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <nav class="main-nav">
-                    <!-- ***** Logo Start ***** -->
-                    <a href="index.html" class="logo">
-                        <img src="assets/images/logo.svg" alt="" style="max-width: 100px;">
-                    </a>
-                    <!-- ***** Logo End ***** -->
-                    <!-- ***** Menu Start ***** -->
-                    <ul class="nav">
-                      <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
-                      <li class="scroll-to-section"><a href="#services">Services</a></li>
-                      <li class="scroll-to-section"><a href="#projects">Projects</a></li>
-                      <li class="has-sub">
-                          <a href="javascript:void(0)">Pages</a>
-                          <ul class="sub-menu">
-                              <li><a href="about.html">About Us</a></li>
-                              <li><a href="faqs.html">FAQs</a></li>
-                          </ul>
-                      </li>
-                      <li class="scroll-to-section"><a href="#infos">Infos</a></li>
-                      <li class="scroll-to-section"><a href="#contact">Contact</a></li>
-                  </ul>   
-                    <a class='menu-trigger'>
-                        <span>Menu</span>
-                    </a>
-                    <!-- ***** Menu End ***** -->
-                </nav>
-            </div>
+      <div class="row">
+        <div class="col-12">
+          <nav class="main-nav">
+            <!-- ***** Logo Start ***** -->
+            <a href="index.html" class="logo">
+              <img src="user/assets/images/logo.svg" alt="" style="max-width: 100px;">
+            </a>
+            <!-- ***** Logo End ***** -->
+            <!-- ***** Menu Start ***** -->
+            <ul class="nav">
+              <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
+              <li class="scroll-to-section"><a href="#services">Services</a></li>
+              <li class="scroll-to-section"><a href="#projects">Projects</a></li>
+              <li class="has-sub">
+                <a href="javascript:void(0)">Pages</a>
+                <ul class="sub-menu">
+                  <li><a href="about.html">About Us</a></li>
+                  <li><a href="faqs.html">FAQs</a></li>
+                </ul>
+              </li>
+              <li class="scroll-to-section"><a href="#infos">Infos</a></li>
+              <li class="scroll-to-section"><a href="#contact">Contact</a></li>
+            </ul>
+            <a class='menu-trigger'>
+              <span>Menu</span>
+            </a>
+            <!-- ***** Menu End ***** -->
+          </nav>
         </div>
+      </div>
     </div>
   </header>
   <div class="projects section" id="projects">
@@ -117,49 +117,49 @@ https://templatemo.com/tm-582-tale-seo-agency
             <p>Find Best Quality Medications</p>
           </div>
         </div>
-      </div> 
+      </div>
     </div>
     <div class="container-fluid">
       <div class="row">
         <div class="col-lg-12">
           <div class="owl-features owl-carousel">
             <div class="item">
-              <img src="assets/images/projects-01.jpg" alt="">
+              <img src="user/assets/images/projects-01.jpg" alt="">
               <div class="down-content">
                 <h4>Digital Agency HTML Templates</h4>
                 <a href="#"><i class="fa fa-link"></i></a>
               </div>
             </div>
             <div class="item">
-              <img src="assets/images/projects-02.jpg" alt="">
+              <img src="user/assets/images/projects-02.jpg" alt="">
               <div class="down-content">
                 <h4>Admin Dashboard CSS Templates</h4>
                 <a href="#"><i class="fa fa-link"></i></a>
               </div>
             </div>
             <div class="item">
-              <img src="assets/images/projects-03.jpg" alt="">
+              <img src="user/assets/images/projects-03.jpg" alt="">
               <div class="down-content">
                 <h4>Best Responsive Website Layouts</h4>
                 <a href="#"><i class="fa fa-link"></i></a>
               </div>
             </div>
             <div class="item">
-              <img src="assets/images/projects-04.jpg" alt="">
+              <img src="user/assets/images/projects-04.jpg" alt="">
               <div class="down-content">
                 <h4>HTML CSS Layouts for your websites</h4>
                 <a href="#"><i class="fa fa-link"></i></a>
               </div>
             </div>
             <div class="item">
-              <img src="assets/images/projects-02.jpg" alt="">
+              <img src="user/assets/images/projects-02.jpg" alt="">
               <div class="down-content">
                 <h4>Bootstrap 5 Themes for Free</h4>
                 <a href="#"><i class="fa fa-link"></i></a>
               </div>
             </div>
             <div class="item">
-              <img src="assets/images/projects-03.jpg" alt="">
+              <img src="user/assets/images/projects-03.jpg" alt="">
               <div class="down-content">
                 <h4>Mobile Friendly Website Layouts</h4>
                 <a href="#"><i class="fa fa-link"></i></a>
@@ -176,42 +176,42 @@ https://templatemo.com/tm-582-tale-seo-agency
         <div class="col-lg-12">
           <div class="owl-features owl-carousel">
             <div class="item">
-              <img src="assets/images/projects-01.jpg" alt="">
+              <img src="user/assets/images/projects-01.jpg" alt="">
               <div class="down-content">
                 <h4>Digital Agency HTML Templates</h4>
                 <a href="#"><i class="fa fa-link"></i></a>
               </div>
             </div>
             <div class="item">
-              <img src="assets/images/projects-02.jpg" alt="">
+              <img src="user/assets/images/projects-02.jpg" alt="">
               <div class="down-content">
                 <h4>Admin Dashboard CSS Templates</h4>
                 <a href="#"><i class="fa fa-link"></i></a>
               </div>
             </div>
             <div class="item">
-              <img src="assets/images/projects-03.jpg" alt="">
+              <img src="user/assets/images/projects-03.jpg" alt="">
               <div class="down-content">
                 <h4>Best Responsive Website Layouts</h4>
                 <a href="#"><i class="fa fa-link"></i></a>
               </div>
             </div>
             <div class="item">
-              <img src="assets/images/projects-04.jpg" alt="">
+              <img src="user/assets/images/projects-04.jpg" alt="">
               <div class="down-content">
                 <h4>HTML CSS Layouts for your websites</h4>
                 <a href="#"><i class="fa fa-link"></i></a>
               </div>
             </div>
             <div class="item">
-              <img src="assets/images/projects-02.jpg" alt="">
+              <img src="user/assets/images/projects-02.jpg" alt="">
               <div class="down-content">
                 <h4>Bootstrap 5 Themes for Free</h4>
                 <a href="#"><i class="fa fa-link"></i></a>
               </div>
             </div>
             <div class="item">
-              <img src="assets/images/projects-03.jpg" alt="">
+              <img src="user/assets/images/projects-03.jpg" alt="">
               <div class="down-content">
                 <h4>Mobile Friendly Website Layouts</h4>
                 <a href="#"><i class="fa fa-link"></i></a>
@@ -228,42 +228,42 @@ https://templatemo.com/tm-582-tale-seo-agency
         <div class="col-lg-12">
           <div class="owl-features owl-carousel">
             <div class="item">
-              <img src="assets/images/projects-01.jpg" alt="">
+              <img src="user/assets/images/projects-01.jpg" alt="">
               <div class="down-content">
                 <h4>Digital Agency HTML Templates</h4>
                 <a href="#"><i class="fa fa-link"></i></a>
               </div>
             </div>
             <div class="item">
-              <img src="assets/images/projects-02.jpg" alt="">
+              <img src="user/assets/images/projects-02.jpg" alt="">
               <div class="down-content">
                 <h4>Admin Dashboard CSS Templates</h4>
                 <a href="#"><i class="fa fa-link"></i></a>
               </div>
             </div>
             <div class="item">
-              <img src="assets/images/projects-03.jpg" alt="">
+              <img src="user/assets/images/projects-03.jpg" alt="">
               <div class="down-content">
                 <h4>Best Responsive Website Layouts</h4>
                 <a href="#"><i class="fa fa-link"></i></a>
               </div>
             </div>
             <div class="item">
-              <img src="assets/images/projects-04.jpg" alt="">
+              <img src="user/assets/images/projects-04.jpg" alt="">
               <div class="down-content">
                 <h4>HTML CSS Layouts for your websites</h4>
                 <a href="#"><i class="fa fa-link"></i></a>
               </div>
             </div>
             <div class="item">
-              <img src="assets/images/projects-02.jpg" alt="">
+              <img src="user/assets/images/projects-02.jpg" alt="">
               <div class="down-content">
                 <h4>Bootstrap 5 Themes for Free</h4>
                 <a href="#"><i class="fa fa-link"></i></a>
               </div>
             </div>
             <div class="item">
-              <img src="assets/images/projects-03.jpg" alt="">
+              <img src="user/assets/images/projects-03.jpg" alt="">
               <div class="down-content">
                 <h4>Mobile Friendly Website Layouts</h4>
                 <a href="#"><i class="fa fa-link"></i></a>
@@ -279,9 +279,10 @@ https://templatemo.com/tm-582-tale-seo-agency
   <footer>
     <div class="container">
       <div class="col-lg-12">
-        <p>Copyright © 2036 <a href="#">Tale SEO Agency</a>. All rights reserved. 
-        
-        <br>Design: <a href="https://templatemo.com" target="_blank">TemplateMo</a></p>
+        <p>Copyright © 2036 <a href="#">Tale SEO Agency</a>. All rights reserved.
+
+          <br>Design: <a href="https://templatemo.com" target="_blank">TemplateMo</a>
+        </p>
       </div>
     </div>
   </footer>
@@ -289,16 +290,16 @@ https://templatemo.com/tm-582-tale-seo-agency
 
   <!-- Scripts -->
   <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+  <script src="user/vendor/jquery/jquery.min.js"></script>
+  <script src="user/vendor/bootstrap/js/bootstrap.min.js"></script>
 
-  <script src="assets/js/isotope.min.js"></script>
-  <script src="assets/js/owl-carousel.js"></script>
-  <script src="assets/js/tabs.js"></script>
-  <script src="assets/js/popup.js"></script>
-  <script src="assets/js/custom.js"></script>
+  <script src="user/assets/js/isotope.min.js"></script>
+  <script src="user/assets/js/owl-carousel.js"></script>
+  <script src="user/assets/js/tabs.js"></script>
+  <script src="user/assets/js/popup.js"></script>
+  <script src="user/assets/js/custom.js"></script>
 
 
-  </body>
+</body>
 
 </html>
