09aef3e2799ec5b15c15a1324aaaaef2bf9e9679
Anass-Dr <anasdrissi77@gmail.com>
2023-12-31 12:52:22 +0100

    toast info changed

--- mvc/views/user/products.php
+++ mvc/views/user/products.php
@@ -162,7 +162,7 @@ https://templatemo.com/tm-582-tale-seo-agency
     <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
       <div class="toast-header">
         <strong class="me-auto">Bootstrap</strong>
-        <small>11 mins ago</small>
+        <small>just now</small>
         <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
       </div>
       <div class="toast-body">
